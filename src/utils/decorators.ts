60e0b9722841577b48b81f7c6ebd37613ef5b632
Sri Panyam <sri.panyam@gmail.com>
2021-02-11 23:03:22 -0800

    Type fixes

--- src/utils/decorators.ts
+++ src/utils/decorators.ts
@@ -1,10 +1,10 @@
-export function mixinUuid() {
-  return function <T extends { new (...args: any[]): {} }>(constructor: T) {
+export function mixinUuid(): any {
+  return function <T extends { new (...args: any[]): Record<string, any> }>(constructor: T): any {
     // return function (constructor: Function) {
     let counter = 0;
     return class extends constructor {
       // Globally unique ID for all elements.
-      readonly uuid = counter++;
+      readonly uuid: number = counter++;
     };
   };
 }
