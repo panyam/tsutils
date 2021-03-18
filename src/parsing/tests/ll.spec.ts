12abbc1f641d3672d7233f308a03d3e3ca3679c7
Sri Panyam <sri.panyam@gmail.com>
2021-03-17 18:44:28 -0700

    Parse table test verified.

--- src/parsing/tests/ll.spec.ts
+++ src/parsing/tests/ll.spec.ts
@@ -5,6 +5,7 @@ import { assert } from "../../utils/misc";
 import { ParseTable, LL1ParseTable } from "../ll";
 import { printGrammar } from "../utils";
 import { expectFSEntries } from "./utils";
+import Samples from "./samples";
 
 function expectPTabEntries(
   g: Grammar,
@@ -15,7 +16,7 @@ function expectPTabEntries(
     const [ntL, termL, ents] = entries[e];
     ents.sort();
     const nt = g.getNT(ntL)!;
-    const term = termL == g.Eof.label ? g.Eof : g.getLit(termL) as Term;
+    const term = termL == g.Eof.label ? g.Eof : (g.getLit(termL) as Term);
     const ptEntry = ptab.get(nt, term) || [];
     ptEntry.sort();
     expect(ptEntry.length).toEqual(ents.length);
@@ -71,6 +72,34 @@ describe("LLParseTable Tests", () => {
       ],
       ["S1", g.Eof.label, [["S1", 1, 0]]],
       ["E", "b", [["E", 0, 0]]],
+    ]);
+  });
+
+  test("Tests 2", () => {
+    const g = new EBNFParser(Samples.expr2).grammar;
+
+    const ns = new NullableSet(g);
+    const fs = new FirstSets(g, ns);
+    const fls = new FollowSets(g, fs);
+    const ptab = new LL1ParseTable(g, fls);
+    expect(ptab.count).toBe(13);
+    expectPTabEntries(g, ptab, [
+      ["E", "id", [["E", 0, 0]]],
+      ["E", "OPEN", [["E", 0, 0]]],
+      ["E1", "PLUS", [["E1", 0, 0]]],
+      ["E1", "CLOSE", [["E1", 1, 0]]],
+      ["E1", g.Eof.label, [["E1", 1, 0]]],
+
+      ["T", "id", [["T", 0, 0]]],
+      ["T", "OPEN", [["T", 0, 0]]],
+
+      ["T1", "PLUS", [["T1", 1, 0]]],
+      ["T1", "STAR", [["T1", 0, 0]]],
+      ["T1", "CLOSE", [["T1", 1, 0]]],
+      ["T1", g.Eof.label, [["T1", 1, 0]]],
+
+      ["F", "id", [["F", 1, 0]]],
+      ["F", "OPEN", [["F", 0, 0]]],
     ]);
   });
 });
