c0f85d9a710178954122cc71deff71144243879c
Sri Panyam <sri.panyam@gmail.com>
2021-08-30 01:37:15 -0700

    Adding circular reference remover for making test printing easy

--- src/misc.ts
+++ src/misc.ts
@@ -58,3 +58,16 @@ export function firstIndexOf<T>(
   }
   return -1;
 }
+
+export const getCircularReplacer = () => {
+  const seen = new WeakSet();
+  return (key: any, value: any) => {
+    if (typeof value === "object" && value !== null) {
+      if (seen.has(value)) {
+        return;
+      }
+      seen.add(value);
+    }
+    return value;
+  };
+};
