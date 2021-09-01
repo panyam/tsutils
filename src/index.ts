c632ba0f03890ff6f7bb515db4c0d7e9b191aab7
Sri Panyam <sri.panyam@gmail.com>
2021-08-31 19:31:55 -0700

    Adding browser checkers

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,14 @@ export * as Num from "./numberutils";
 export * as Time from "./timeutils";
 // export * as Properties from "./properties";
 export * as DAL from "./dal";
+
+export class Browser {
+  static IS_EXPLORER = navigator.userAgent.indexOf("MSIE") > -1;
+  static IS_FIREFOX = navigator.userAgent.indexOf("Firefox") > -1;
+  static IS_OPERA = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+
+  protected static UAHasChrome = navigator.userAgent.indexOf("Chrome") > -1;
+  protected static UAHasSafari = navigator.userAgent.indexOf("Safari") > -1;
+  static IS_SAFARI = Browser.UAHasSafari && (!Browser.UAHasChrome || !Browser.UAHasSafari);
+  static IS_CHROME = Browser.UAHasChrome && (!Browser.UAHasChrome || !Browser.IS_OPERA);
+}
