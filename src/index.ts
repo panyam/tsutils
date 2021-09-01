703817b643a4fe45179138071d78c43ace1f8525
Sri Panyam <sri.panyam@gmail.com>
2021-08-31 19:42:10 -0700

    Ensure navigator is set on Browser checks

--- src/index.ts
+++ src/index.ts
@@ -11,13 +11,14 @@ export * as Time from "./timeutils";
 // export * as Properties from "./properties";
 export * as DAL from "./dal";
 
+declare const navigator: any;
 export class Browser {
-  static IS_EXPLORER = navigator.userAgent.indexOf("MSIE") > -1;
-  static IS_FIREFOX = navigator.userAgent.indexOf("Firefox") > -1;
-  static IS_OPERA = navigator.userAgent.toLowerCase().indexOf("op") > -1;
+  static IS_EXPLORER = navigator && navigator.userAgent.indexOf("MSIE") > -1;
+  static IS_FIREFOX = navigator && navigator.userAgent.indexOf("Firefox") > -1;
+  static IS_OPERA = navigator && navigator.userAgent.toLowerCase().indexOf("op") > -1;
 
-  protected static UAHasChrome = navigator.userAgent.indexOf("Chrome") > -1;
-  protected static UAHasSafari = navigator.userAgent.indexOf("Safari") > -1;
-  static IS_SAFARI = Browser.UAHasSafari && (!Browser.UAHasChrome || !Browser.UAHasSafari);
-  static IS_CHROME = Browser.UAHasChrome && (!Browser.UAHasChrome || !Browser.IS_OPERA);
+  protected static UAHasChrome = navigator && navigator.userAgent.indexOf("Chrome") > -1;
+  protected static UAHasSafari = navigator && navigator.userAgent.indexOf("Safari") > -1;
+  static IS_SAFARI = navigator && Browser.UAHasSafari && (!Browser.UAHasChrome || !Browser.UAHasSafari);
+  static IS_CHROME = navigator && Browser.UAHasChrome && (!Browser.UAHasChrome || !Browser.IS_OPERA);
 }
